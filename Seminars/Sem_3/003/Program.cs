a523c8eaf33b83bf5798dbf38e40d9ae11476f8b
Sys Darya <sysdaryaandreevna@gmail.com>
2022-10-07 22:34:28 +0300

    task003

--- Seminars/Sem_3/003/Program.cs
+++ Seminars/Sem_3/003/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// Math.Sqrt() // команда квадратный корень
+
+const int X = 0;
+const int Y = 1;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[] InputPoint(int point)
+{
+    int[] answer = new int[2];
+    answer[X] = Prompt($"Введите x {point} -> ");
+    answer[Y] = Prompt($"Введите y {point} -> ");
+    return answer;
+}
+int Power2(int arg)
+{
+    return arg * arg;
+}
+int[] p1 = InputPoint(1);
+int[] p2 = InputPoint(2);
+
+double lenght = Math.Sqrt(Power2(p1[X] - p2[X]) + Power2(p1[Y] - p2[Y])); // <- Math.Sqrt
+
+System.Console.WriteLine($"{lenght:f2}"); // f - количество знаков после запятой.
