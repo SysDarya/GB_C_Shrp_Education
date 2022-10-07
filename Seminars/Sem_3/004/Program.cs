8186d16007da8e83561fdc01b85b3abed3cc8d54
Sys Darya <sysdaryaandreevna@gmail.com>
2022-10-07 22:35:16 +0300

    task004

--- Seminars/Sem_3/004/Program.cs
+++ Seminars/Sem_3/004/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int N = Prompt("Введите число: ");
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(Math.Pow(i,2));
+}
