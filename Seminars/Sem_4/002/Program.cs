b02ba8dd46644800d0693743c63fa1c70fdfb766
Sys Darya <sysdaryaandreevna@gmail.com>
2022-10-11 21:11:25 +0300

    task002

---
+++ Seminars/Sem_4/002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+int InputNumber(string msg)
+{
+    System.Console.WriteLine(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Count(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        result++;
+    }
+    return result;
+}
+
+int number = InputNumber("Введите число: ");
+int result = Count(number);
+System.Console.WriteLine($"{result}");
+
