3cf1402ebd77e05cb5f79f4c698886da93cef6a2
Sys Darya <sysdaryaandreevna@gmail.com>
2022-10-14 18:45:06 +0300

    task003

--- Hometasks/Hometask4/003/Program.cs
+++ Hometasks/Hometask4/003/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+
+void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-100,101);
+        index++;
+    }
+}
+
+void PrintArray (int[] collection)
+{
+    int count = collection.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write ($"{collection[position]}\t");
+        position++;
+    }
+}
+
+int [] array = new int [8];
+FillArray(array);
+PrintArray(array);
