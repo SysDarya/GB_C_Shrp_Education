f8d64219c79e954be4422288bad0c999bb4f835f
Sys Darya <sysdaryaandreevna@gmail.com>
2022-10-18 22:00:43 +0300

    task005

---
+++ Seminars/Sem_6/004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+
+int tmp1 = 0;
+int tmp2 = 1;
+
+while(count  > 0)
+{
+   int tmp = tmp1 + tmp2;
+   System.Console.WriteLine($"{tmp1}");
+   tmp1 = tmp2;
+   tmp2 = tmp;
+   count--;
+}
+
