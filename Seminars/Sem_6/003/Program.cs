a3c83cb648f7bd60ad1102fdee562b70c73f2f3a
Sys Darya <sysdaryaandreevna@gmail.com>
2022-10-18 21:22:49 +0300

    task003

--- Seminars/Sem_6/003/Program.cs
+++ Seminars/Sem_6/003/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+int Prompt()
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+int[] Binarny(int number)
+{
+    int[] array = new int[10];
+    int count = array.Length - 1;
+    while (number > 0)
+    { 
+        array[count] = number % 2;
+        number /= 2;
+        count --;
+    } 
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int a = Prompt();
+PrintArray(Binarny(a));
