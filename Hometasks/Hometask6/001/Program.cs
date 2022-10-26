3947341630f8a6aa792a3fb6485796eb4a871b7a
Sys Darya <sysdaryaandreevna@gmail.com>
2022-10-26 17:32:03 +0300

    task001

---
+++ Hometasks/Hometask6/001/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Разделить ввод данных, от расчета данных (Одна функция вводит данные, другая функция подсчитывает количество положительных)
+
+int[] NumbersInArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for(int c = 0; c < array.Length; c++)
+    {
+        if (array[c] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+Console.Write("Какое количество чисел вы хотите проверить? > ");
+int M = Convert.ToInt32(Console.ReadLine());
+if (M > 0)
+{
+    int[] array = new int [M];
+    Console.WriteLine("Введите ваши числа > ");
+    NumbersInArray(array);
+    int positive = CountPositive(array);
+    Console.WriteLine($"Количество положительных чисел равно {positive}.");
+}
+else
+{
+    Console.WriteLine("ОШИБКА!!! Похоже, вы ввели число, равное нулю или меньше него. Попробуйте снова!");
+}
