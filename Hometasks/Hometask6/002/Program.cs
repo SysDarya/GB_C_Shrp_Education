7b35a2e6b27382cf41cc44524114865d8605f6ab
Sys Darya <sysdaryaandreevna@gmail.com>
2022-10-26 18:00:22 +0300

    task002

--- Hometasks/Hometask6/002/Program.cs
+++ Hometasks/Hometask6/002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+float Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+var k1 = Prompt("Введите k1 > ");
+var b1 = Prompt("Введите b1 > ");
+var k2 = Prompt("Введите k2 > ");
+var b2 = Prompt("Введите b2 > ");
+
+var x = (b2 - b1)/(k1 - k2);
+var y = k1 * x + b1;
+
+Console.WriteLine($"Прямые пересекаются в точке: ({x};{y}.)");
